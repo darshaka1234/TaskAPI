7ba61ca11ab5af8fc026ea727c23915d1bad43cb
darshaka1234 <darshakasrimal1234@gmail.com>
2023-02-05 13:24:37 +0530

    model added

--- TaskAPI/Controllers/TodosController.cs
+++ TaskAPI/Controllers/TodosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TaskAPI.Models;
 
 namespace TaskAPI.Controllers
 {
@@ -7,11 +8,41 @@ namespace TaskAPI.Controllers
     [ApiController]
     public class TodosController : ControllerBase
     {
-        [HttpGet]   
-        public IActionResult Todos()
+        [HttpGet]
+        public IActionResult GetTodos()
         {
-            var todos = new String[] { "task1", "task2", "task3" };
+            var todos = AllTodos();
             return Ok(todos);
         }
+        private static List<Todo> AllTodos()
+        {
+            var todos = new List<Todo>();
+
+            var todo1 = new Todo
+            {
+                Id = 1,
+                Title = "todo1",
+                Description = "just todo 1",
+                Created = DateTime.Now,
+                Due = DateTime.Now.Date.AddDays(1),
+                Status = TodoStatus.New
+            };
+            todos.Add(todo1);
+
+            var todo2 = new Todo
+            {
+                Id = 2,
+                Title = "todo2",
+                Description = "just todo 2",
+                Created = DateTime.Now,
+                Due = DateTime.Now.Date.AddDays(2),
+                Status = TodoStatus.New
+            };
+            todos.Add(todo1);
+
+            return todos;
+        }
     }
+    
 }
+
