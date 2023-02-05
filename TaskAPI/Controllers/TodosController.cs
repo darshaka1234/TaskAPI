1d8872a84c8d0cced4c20f9efcd5987269be52f5
darshaka1234 <darshakasrimal1234@gmail.com>
2023-02-05 12:48:13 +0530

    Add project files.

---
+++ TaskAPI/Controllers/TodosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        [HttpGet]   
+        public IActionResult Todos()
+        {
+            var todos = new String[] { "task1", "task2", "task3" };
+            return Ok(todos);
+        }
+    }
+}
