e5dc8efdd7099170dd0ca6ac02ce4388a8ffd320
darshaka1234 <darshakasrimal1234@gmail.com>
2023-02-06 15:07:36 +0530

    server injected

--- TaskAPI/Program.cs
+++ TaskAPI/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ItodoRepository, TodoServices>();    
+builder.Services.AddScoped<ItodoRepository, TodoSqlServerService>();    
 
 var app = builder.Build();
 
