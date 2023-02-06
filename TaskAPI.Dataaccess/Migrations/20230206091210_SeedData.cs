1717b1725b567c5eaf308cfcdbf3ca534508fd41
darshaka1234 <darshakasrimal1234@gmail.com>
2023-02-06 14:47:19 +0530

    Sql server connected

---
+++ TaskAPI.Dataaccess/Migrations/20230206091210_SeedData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskAPI.Dataaccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Todos",
+                columns: new[] { "Id", "Created", "Description", "Due", "Status", "Title" },
+                values: new object[] { 1, new DateTime(2023, 2, 6, 14, 42, 9, 822, DateTimeKind.Local).AddTicks(8193), "just todo 1 from database", new DateTime(2023, 2, 7, 0, 0, 0, 0, DateTimeKind.Local), 0, "todo1" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
---
+++ TaskAPI.Dataaccess/TodoDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskAPI.Models;
+
+namespace TaskAPI.Dataaccess
+{
+    public class TodoDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\MSSQLSERVER01;Database=master;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>().HasData(new Todo{
+                Id = 1,
+                Title = "todo1",
+                Description = "just todo 1 from database",
+                Created = DateTime.Now,
+                Due = DateTime.Now.Date.AddDays(1),
+                Status = TodoStatus.New
+            });
+        }
+    }
+}
